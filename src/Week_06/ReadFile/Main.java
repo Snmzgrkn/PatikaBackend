de9c538882885eb98b65810a6b5875583fd3fb35
Gürkan Sönmez <snmzgrkn@gmail.com>
2024-03-27 16:26:46 +0300

    27.03.2024

---
+++ src/Week_06/ReadFile/Main.java
@@ -0,0 +1,26 @@
+package Week_06.ReadFile;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            String path = "..........\\ReadFile\\deneme.txt";
+            File file = new File(path);
+
+            FileReader fReader = new FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader(fReader);
+            String line;
+            int total =0;
+            while ((line = bufferedReader.readLine()) != null) {
+                total+= Integer.parseInt(line);
+            }
+            System.out.println(total);
+        } catch (Exception e) {
+            e.getStackTrace();
+        }
+
+    }
+}
